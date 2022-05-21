d5b67c544d079d639824fef31f4024ada6884188
Coolguy6316 <coolguy6318@hotmail.com>
2022-05-21 10:41:42 +0200

    Player State Switching Setup

---
+++ Assets/Scripts/PlayerStates.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStates : MonoBehaviour
+{
+    public enum PLAYERSTATES { Vampire, Chicken, Sheep }
+
+    //This is the same as using a private var and public getter but in 1 line.
+    [field: Header("State")]
+    [field: SerializeField] public PLAYERSTATES CurrentState { get; private set; }
+
+    [Header("Sprites")]
+    [SerializeField] private Sprite vampireSprite;
+    [SerializeField] private Sprite chickenSprite;
+    [SerializeField] private Sprite sheepSprite;
+
+    private SpriteRenderer playerRenderer;
+    private BoxCollider2D playerCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRenderer = GetComponent<SpriteRenderer>();
+        playerCollider = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //TODO: Temp inputs for testing, remove later
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            SwitchState(PLAYERSTATES.Vampire);
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            SwitchState(PLAYERSTATES.Chicken);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            SwitchState(PLAYERSTATES.Sheep);
+        }
+    }
+
+    //Call this method from other scripts to switch state
+    public void SwitchState(PLAYERSTATES state)
+    {
+        CurrentState = state;
+
+        switch (CurrentState)
+        {
+            case PLAYERSTATES.Vampire:
+                playerRenderer.sprite = vampireSprite;
+                break;
+            case PLAYERSTATES.Chicken:
+                playerRenderer.sprite = chickenSprite;
+                break;
+            case PLAYERSTATES.Sheep:
+                playerRenderer.sprite = sheepSprite;
+                break;
+            default:
+                break;
+        }
+
+        ResizeCollider();
+    }
+
+    void ResizeCollider()
+    {
+        playerCollider.offset = new Vector2(0, 0);
+        playerCollider.size = new Vector3(playerRenderer.bounds.size.x / transform.lossyScale.x,
+                                     playerRenderer.bounds.size.y / transform.lossyScale.y,
+                                     playerRenderer.bounds.size.z / transform.lossyScale.z);
+    }
+}
